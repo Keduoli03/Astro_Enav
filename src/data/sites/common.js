3c7063bd7487c3e8eff5e9ef755d7b1dbbd0603b
Lanke <129925425+Keduoli03@users.noreply.github.com>
2025-08-04 13:06:19 +0800

    Update common.js

--- src/data/sites/common.js
+++ src/data/sites/common.js
@@ -21,14 +21,27 @@ export default [
     logo: "/images/logos/vercel.ico",
     id: "Vercel"
   },
+   {
+    url: "https://umami.blueke.top/dashboard",
+    title: "Umami",
+    description: "自部署Umami站点监测",
+    logo: "",
+    id: "Umami"
+  },
+  {
+    url: "https://reader.blueke.top/i/",
+    title: "Rss阅读器",
+    description: "自部署FreshRss阅读器",
+    logo: "",
+    id: "FreshRss"
+  },
   {
     url: "https://test.ustc.edu.cn/",
     title: "中科大测速网站",
     description: "中国科技大学测速网站，要来测个速吗",
     logo: "/images/logos/test.ustc.edu.cn-favicon.ico",
     id: "ustc"
-  }
-  ,
+  },
   {
     url: "https://favicon.im/zh",
     title: "网站图标获取",
