88c40601f001dbdee7a0848a71c6eaaab1a187bb
Lanke <2801429414@qq.com>
2025-11-22 21:46:53 +0800

    Update all.js
    更新一下我的网站，有的废弃了

--- src/data/sites/all.js
+++ src/data/sites/all.js
@@ -1,79 +1,280 @@
 export default [
-  // common
-  { subId: 'common', url: 'https://www.blueke.top', title: '主页', description: '作者主页，欢迎来访！', id: 'yuque' },
-  { subId: 'common', url: 'https://blog.blueke.top', title: '博客', description: '作者Blog，欢迎来访！', logo: 'images/logo.png', id: 'yuque' },
-  { subId: 'common', url: 'https://www.doubao.com/chat', title: '豆包', description: '豆包Chat，一个基于大模型的智能问答平台', logo: '', id: 'doubao' },
-  { subId: 'common', url: 'https://github.com', title: 'GitHub', description: '代码托管平台', logo: '/images/logos/github.jpg', id: 'github' },
-  { subId: 'common', url: 'https://vercel.com/', title: 'Vercel', description: '代码托管平台,我许多项目都托管到上面啦!', logo: '/images/logos/vercel.ico', id: 'Vercel' },
-  { subId: 'common', url: 'https://umami.blueke.top/dashboard', title: 'Umami', description: '自部署Umami站点监测', logo: '', id: 'Umami' },
-  { subId: 'common', url: 'https://reader.blueke.top/i/', title: 'Rss阅读器', description: '自部署FreshRss阅读器', logo: '', id: 'FreshRss' },
-  { subId: 'common', url: 'https://memo.blueke.top/', title: 'MissKey', description: '自部署MissKey', id: 'MissKey' },
-  { subId: 'common', url: 'https://test.ustc.edu.cn/', title: '中科大测速网站', description: '中国科技大学测速网站，要来测个速吗', logo: '/images/logos/test.ustc.edu.cn-favicon.ico', id: 'ustc' },
-  { subId: 'common', url: 'https://favicon.im/zh', title: '网站图标获取', description: '即时获取和下载任何网站的图标。', logo: '/images/logos/favicon.im.ico', id: 'ustc' },
+    // 常用工具
+    {
+        subId: 'common',
+        url: 'https://www.blueke.top',
+        title: '主页',
+        description: '作者主页，欢迎来访！',
+        id: 'yuque'
+    },
+    {
+        subId: 'common',
+        url: 'https://blog.blueke.top',
+        title: '博客',
+        description: '作者Blog，欢迎来访！',
+        logo: 'images/logo.png',
+        id: 'yuque'
+    },
+    {
+        subId: 'common',
+        url: 'https://www.doubao.com/chat',
+        title: '豆包',
+        description: '豆包Chat，一个基于大模型的智能问答平台',
+        logo: '',
+        id: 'doubao'
+    },
+    {
+        subId: 'common',
+        url: 'https://github.com',
+        title: 'GitHub',
+        description: '代码托管平台',
+        logo: '',
+        id: 'github'
+    },
+    {
+        subId: 'common',
+        url: 'https://vercel.com/',
+        title: 'Vercel',
+        description: '代码托管平台,我许多项目都托管到上面啦!',
+        logo: '',
+        id: 'Vercel'
+    },
+    {
+        subId: 'common',
+        url: 'https://memos.blueke.top/',
+        title: 'Memos',
+        description: '自部署Memos',
+        id: 'memos'
+    },
+    {
+        subId: 'common',
+        url: 'https://test.ustc.edu.cn/',
+        title: '中科大测速网站',
+        description: '中国科技大学测速网站，要来测个速吗',
+        logo: '',
+        id: 'ustc'
+    },
+    {
+        subId: 'common',
+        url: 'https://favicon.im/zh',
+        title: '网站图标获取',
+        description: '即时获取和下载任何网站的图标。',
+        logo: '',
+        id: 'favicon'
+    },
 
-  // anime
-  { subId: 'anime', url: 'https://mikanani.me/', title: '蜜柑计划', description: '首推，收录了很多字幕组的动漫资源', logo: 'images/logos/mikanani.ico', id: 'mikanani' },
-  { subId: 'anime', url: 'https://animes.garden/', title: '动漫花园镜像站', description: '非常全的一个字幕组资源搜索引擎', logo: '', id: 'dmhyjx' },
-  { subId: 'anime', url: 'https://bangumi.moe/', title: '萌番组', description: '传统BT动漫资源网站，收录了很多字幕组的动漫资源', logo: '', id: 'bangumi' },
-  { subId: 'anime', url: 'https://acg.rip/', title: 'ACG.RIP', description: '传统BT动漫资源网站，收录了很多字幕组的动漫资源', logo: 'images/logos/acgrip.ico', id: 'acgrip' },
-  { subId: 'anime', url: 'https://share.dmhy.org/', title: '动漫花园', description: '传统BT动漫资源网站，收录了很多字幕组的动漫资源', logo: 'images/logos/dmhy.ico', id: 'dmhy' },
-  {
-    subId: 'anime',
-    url: 'https://miraitv.netlify.app/',
-    title: 'miraitv',
-    description: '群友写的看番网站,貌似体验不错',
-    logo: '',
-    id: 'miraitv'
-  },
+    // anime
+    {
+        subId: 'anime',
+        url: 'https://mikanani.me/',
+        title: '蜜柑计划',
+        description: '首推，收录了很多字幕组的动漫资源',
+        logo: '',
+        id: 'mikanani'
+    },
+    {
+        subId: 'anime',
+        url: 'https://animes.garden/',
+        title: '动漫花园镜像站',
+        description: '非常全的一个字幕组资源搜索引擎',
+        logo: '',
+        id: 'dmhyjx'
+    },
+    {
+        subId: 'anime',
+        url: 'https://bangumi.moe/',
+        title: '萌番组',
+        description: '传统BT动漫资源网站，收录了很多字幕组的动漫资源',
+        logo: '',
+        id: 'bangumi'
+    },
+    {
+        subId: 'anime',
+        url: 'https://acg.rip/',
+        title: 'ACG.RIP',
+        description: '传统BT动漫资源网站，收录了很多字幕组的动漫资源',
+        logo: 'images/logos/acgrip.ico',
+        id: 'acgrip'
+    },
+    {
+        subId: 'anime',
+        url: 'https://share.dmhy.org/',
+        title: '动漫花园',
+        description: '传统BT动漫资源网站，收录了很多字幕组的动漫资源',
+        logo: 'images/logos/dmhy.ico',
+        id: 'dmhy'
+    },
+    {
+        subId: 'anime',
+        url: 'https://miraitv.netlify.app/',
+        title: 'miraitv',
+        description: '群友写的看番网站,貌似体验不错',
+        logo: '',
+        id: 'miraitv'
+    },
 
-  // animephoto
-  { subId: 'animephoto', url: 'https://www.someacg.top/', title: 'SomeACG', description: '动漫壁纸网站，质量不错', logo: 'images/logos/someacg.png', id: 'someacg' },
-  { subId: 'animephoto', url: 'https://konachan.net/', title: 'konachan.net', description: '动漫壁纸网站，质量还行', logo: 'images/logos/konachan.net.ico', id: 'konachan' },
+    // animephoto
+    {
+        subId: 'animephoto',
+        url: 'https://www.someacg.top/',
+        title: 'SomeACG',
+        description: '动漫壁纸网站，质量不错',
+        logo: 'images/logos/someacg.png',
+        id: 'someacg'
+    },
+    {
+        subId: 'animephoto',
+        url: 'https://konachan.net/',
+        title: 'konachan.net',
+        description: '动漫壁纸网站，质量还行',
+        logo: 'images/logos/konachan.net.ico',
+        id: 'konachan'
+    },
 
-  // cloud
-  { subId: 'cloud', url: 'https://www.pansou.vip/', title: '盘搜', description: '一个优秀的网盘搜索聚合，收录了很多搜索引擎', logo: 'images/logos/pansou.ico', id: 'pansou' },
-  {
-    subId: 'cloud',
-    url: 'https://www.upyunso.com/',
-    title: 'up云搜',
-    description: '一个优秀的网盘搜索聚合',
-    logo: '',
-    id: 'upyunso'
-  },
+    // cloud
+    {
+        subId: 'cloud',
+        url: 'https://www.pansou.vip/',
+        title: '盘搜',
+        description: '一个优秀的网盘搜索聚合，收录了很多搜索引擎',
+        logo: 'images/logos/pansou.ico',
+        id: 'pansou'
+    },
+    {
+        subId: 'cloud',
+        url: 'https://www.upyunso.com/',
+        title: 'up云搜',
+        description: '一个优秀的网盘搜索聚合',
+        logo: '',
+        id: 'upyunso'
+    },
 
-  // editor
-  { subId: 'editor', url: 'https://jetbrains.ankio.net/license', title: 'jetbrains许可证生成器', description: '可以使用绝大多数的JetBrains产品', logo: 'images/logos/jetbrains.png', id: 'pansou' },
-  { subId: 'editor', url: 'https://javaguide.cn/home.html', title: 'Java学习&面试指南', description: '后期java快速学习指南', logo: '', id: 'javaguide' },
+    // editor
+    {
+        subId: 'editor',
+        url: 'https://jetbrains.ankio.net/license',
+        title: 'jetbrains许可证生成器',
+        description: '可以使用绝大多数的JetBrains产品',
+        logo: 'images/logos/jetbrains.png',
+        id: 'pansou'
+    },
+    {
+        subId: 'editor',
+        url: 'https://javaguide.cn/home.html',
+        title: 'Java学习&面试指南',
+        description: '后期java快速学习指南',
+        logo: '',
+        id: 'javaguide'
+    },
 
-  // musictool
-  { subId: 'musictool', url: 'https://www.kanxizai.cn/163_music/', title: '网易云音乐解析', description: '解析下载网易云音乐', id: '163music' },
-  {
-    subId:'musictool',
-    url: 'https://iigeek.com/',
-    title: '音乐解锁',
-    description: '解锁多平台音乐加密',
-    logo: '',
-    id: 'iigeek'
-  },
+    // musictool
+    {
+        subId: 'musictool',
+        url: 'https://www.kanxizai.cn/163_music/',
+        title: '网易云音乐解析',
+        description: '解析下载网易云音乐',
+        id: '163music'
+    },
+    {
+        subId: 'musictool',
+        url: 'https://iigeek.com/',
+        title: '音乐解锁',
+        description: '解锁多平台音乐加密',
+        logo: '',
+        id: 'iigeek'
+    },
 
-  // picturetool
-  { subId: 'picturetool', url: ' https://remove.bg', title: '快速抠图', description: '快速扣透明图，去除背景', logo: '', id: 'koutu' },
-  { subId: 'picturetool', url: 'https://eimg.yujiay.wang/', title: '小E图床', description: '免费稳定的图床工具', logo: '', id: 'eimg' },
+    // picturetool
+    {
+        subId: 'picturetool',
+        url: ' https://remove.bg',
+        title: '快速抠图',
+        description: '快速扣透明图，去除背景',
+        logo: '',
+        id: 'koutu'
+    },
+    {
+        subId: 'picturetool',
+        url: 'https://eimg.yujiay.wang/',
+        title: '小E图床',
+        description: '免费稳定的图床工具',
+        logo: '',
+        id: 'eimg'
+    },
 
-  // reader
-  { subId: 'reader', url: 'https://zh.z-library.sk/', title: 'Z-Library', description: '收录了世界上海量的书籍', logo: 'images/logos/zlib.ico', id: 'zlib' },
-  { subId: 'reader', url: 'https://zh.annas-archive.org/', title: '安娜的橱窗', description: '人类历史上最大的开放完全的图书馆，免费下载速度较慢', logo: 'images/logos/annas.ico', id: 'annas' },
+    // reader
+    {
+        subId: 'reader',
+        url: 'https://zh.z-library.sk/',
+        title: 'Z-Library',
+        description: '收录了世界上海量的书籍',
+        logo: 'images/logos/zlib.ico',
+        id: 'zlib'
+    },
+    {
+        subId: 'reader',
+        url: 'https://zh.annas-archive.org/',
+        title: '安娜的橱窗',
+        description: '人类历史上最大的开放完全的图书馆，免费下载速度较慢',
+        logo: 'images/logos/annas.ico',
+        id: 'annas'
+    },
 
-  // subtitle
-  { subId: 'subtitle', url: 'https://assrt.net/', title: '射手网', description: '非常不错的字幕网站，很多资源', logo: 'images/logos/assrt.png', id: 'assrt' },
-  { subId: 'subtitle', url: 'https://zimuku.org/', title: '字幕库', description: '很多优质的字幕', logo: '', id: 'zimuku' },
-  { subId: 'subtitle', url: 'https://www.opensubtitles.com/', title: 'opensubtitles', description: '备选，用的不多，一天20个下载名额', logo: '', id: 'opensubtitles' },
+    // subtitle
+    {
+        subId: 'subtitle',
+        url: 'https://assrt.net/',
+        title: '射手网',
+        description: '非常不错的字幕网站，很多资源',
+        logo: 'images/logos/assrt.png',
+        id: 'assrt'
+    },
+    {
+        subId: 'subtitle',
+        url: 'https://zimuku.org/',
+        title: '字幕库',
+        description: '很多优质的字幕',
+        logo: '',
+        id: 'zimuku'
+    },
+    {
+        subId: 'subtitle',
+        url: 'https://www.opensubtitles.com/',
+        title: 'opensubtitles',
+        description: '备选，用的不多，一天20个下载名额',
+        logo: '',
+        id: 'opensubtitles'
+    },
 
-  // website
-  { subId: 'website', url: 'https://pagespeed.web.dev/', title: 'Google PageSpeed Insights', description: '分析网站性能', id: 'pagespeed' },
-  { subId: 'website', url: 'https://gtmetrix.com/', title: 'GTmetrix', description: '分析网站性能', id: 'gtmetrix' },
-  { subId: 'website', url: 'https://www.17ce.com/', title: '17ce', description: '国内网站性能分析', id: '17ce' },
+    // website
+    {
+        subId: 'website',
+        url: 'https://pagespeed.web.dev/',
+        title: 'Google PageSpeed Insights',
+        description: '分析网站性能',
+        id: 'pagespeed'
+    },
+    {
+        subId: 'website',
+        url: 'https://gtmetrix.com/',
+        title: 'GTmetrix',
+        description: '分析网站性能',
+        id: 'gtmetrix'
+    },
+    {
+        subId: 'website',
+        url: 'https://www.17ce.com/',
+        title: '17ce',
+        description: '国内网站性能分析',
+        id: '17ce'
+    },
 
-  // 影视网站
-  { subId: '影视网站', url: 'https://www.319312.com/dongman', title: '319影视', description: '收录了很多影视资源', id: '319312' },
+    // 影视网站
+    {
+        subId: '影视网站',
+        url: 'https://www.319312.com/dongman',
+        title: '319影视',
+        description: '收录了很多影视资源',
+        id: '319312'
+    },
 ]
